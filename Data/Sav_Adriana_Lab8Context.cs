08855211d49b78e2bda1e45cfbe033127f977ca2
Adriana Sav <adriana.sav11@gmail.com>
2021-11-23 15:32:10 +0200

    Add project files.

---
+++ Data/Sav_Adriana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sav_Adriana_Lab8.Models;
+
+namespace Sav_Adriana_Lab8.Data
+{
+    public class Sav_Adriana_Lab8Context : DbContext
+    {
+        public Sav_Adriana_Lab8Context (DbContextOptions<Sav_Adriana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sav_Adriana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Sav_Adriana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Sav_Adriana_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sav_Adriana_Lab8.Data;
+using Sav_Adriana_Lab8.Models;
+
+namespace Sav_Adriana_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Sav_Adriana_Lab8.Data.Sav_Adriana_Lab8Context _context;
+
+        public IndexModel(Sav_Adriana_Lab8.Data.Sav_Adriana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
